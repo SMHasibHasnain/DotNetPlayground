ee345e108549eee2723232ade7545824a9de7e8d
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-09-01 13:23:11 +0600

    Add initial implementation of discount application logic with Program and Discount classes

---
+++ Delegates/01_Discounts/Program.cs
@@ -0,0 +1,44 @@
+﻿class Program
+{
+
+    public static void Main(String[] args)
+    {
+        ApplyDiscount applyDiscount = new ApplyDiscount();
+        Discount discount = new Discount();
+
+        applyDiscount.Apply(discount.RegularDiscount, 20, 15);
+        applyDiscount.Apply(discount.VIPDiscount, 30, 40);
+
+    }
+}
+
+class ApplyDiscount()
+{
+
+    public delegate double voucher(double min, double max);
+    public void Apply(voucher discount, double max, double min)
+    {
+        double amount = discount(max, min);
+        System.Console.WriteLine(amount);
+    }
+}
+
+
+class Discount
+{
+    public double RegularDiscount(double min, double max)
+    {
+        return DiscountCalculation(min, max);
+    }
+
+    public double VIPDiscount(double min, double max)
+    {
+        return DiscountCalculation(min, max);
+    }
+
+    public double DiscountCalculation(double min, double max)
+    {
+        double random = Random.Shared.NextDouble();
+        return min + (random * (max-min));
+    }
+}
