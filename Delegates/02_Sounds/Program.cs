fdf0daea10f9cfab2b487dca9c3f5b2a6f32bc84
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-09-01 13:50:39 +0600

    Add initial implementation of animal sound functionality with Program, Animal, and Sounds classes

---
+++ Delegates/02_Sounds/Program.cs
@@ -0,0 +1,39 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal animal = new Animal();
+        Sounds sounds = new Sounds();
+
+        animal.Sound(sounds.Cat);
+        animal.Sound(sounds.Tiger);
+        animal.Sound(sounds.Cow);
+    }
+}
+
+class Animal
+{
+    public delegate void AnimalSound();
+    public void Sound(AnimalSound sound)
+    {
+        sound();
+    }
+}
+
+class Sounds
+{
+    public void Tiger()
+    {
+        System.Console.WriteLine("Halum.... Halum....");
+    }
+
+    public void Cat()
+    {
+        System.Console.WriteLine("Meoww.... Meoww....");
+    }
+
+    public void Cow()
+    {
+        System.Console.WriteLine("Hambaa.... Hambaa....");
+    }
+}
