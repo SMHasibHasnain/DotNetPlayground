041454294211011a9341dfcc2772d9bbdd61142b
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-09-01 15:12:32 +0600

    Add initial implementation of calculator functionality with input parsing and operation handling

---
+++ Delegates/03_Calculator/Program.cs
@@ -0,0 +1,36 @@
+﻿bool isNum1InputValid = double.TryParse(Console.ReadLine(), out double num1);
+bool isOperatorInputValid = char.TryParse(Console.ReadLine(), out char opt);
+bool isNum2InputValid = double.TryParse(Console.ReadLine(), out double num2);
+
+Dictionary<char, Func<double, double, double>> pairs = new Dictionary<char, Func<double, double, double>>();
+
+pairs['+'] = (num1, num2) =>
+{
+    return num1 + num2;
+};
+
+pairs['-'] = (num1, num2) =>
+{
+    return num1 - num2;
+};
+
+pairs['*'] = (num1, num2) =>
+{
+    return num1 * num2;
+};
+
+pairs['/'] = (num1, num2) =>
+{
+    return num1 / num2;
+};
+
+var operation = pairs[opt];
+
+var result = operation(num1, num2);
+
+Action<double> print = (result) =>
+{
+    System.Console.WriteLine($"{num1} {opt} {num2} = {result}");
+};
+
+print(result);
