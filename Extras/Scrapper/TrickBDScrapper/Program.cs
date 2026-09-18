4db59486f248e1ed5203b32f121b209b01bf9c5b
S. M. Hasib Hasnain <hasnain23105341116@diu.edu.bd>
2026-09-18 23:12:04 +0600

    Add initial implementation of TrickBDScrapper with HTML parsing functionality

---
+++ Extras/Scrapper/TrickBDScrapper/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Reflection.Metadata;
+using HtmlAgilityPack;
+
+using var client = new HttpClient();
+
+try
+{
+    var url = "https://techcrunch.com";
+    var html = await client.GetStringAsync(url);   
+    Console.WriteLine($"{url} has been downloaded!");
+    
+    var doc = new HtmlDocument();
+    doc.LoadHtml(html);
+
+    var node = doc.DocumentNode.SelectNodes("//div[@data-custom-module='Latest News']//h3//a[@class='loop-card__title-link']");
+    
+    int i = 0;
+    foreach (var item in node!)
+    {
+        i++;
+        Console.WriteLine( $"[{i}]" + "---> " + item.InnerText);
+    }
+} 
+catch(HttpRequestException ex)
+{
+    System.Console.WriteLine("Sorry!");
+    Console.WriteLine(ex.Message);
+}
+
